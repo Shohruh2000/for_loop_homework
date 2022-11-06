2612018e8bd94eb5fa900d7a9c3a0520c27aad00
Shohruh2000 <shertoyloqovshohruh524@gmail.com>
2022-11-06 19:31:27 +0500

    For03.py

--- For03.py
+++ For03.py
@@ -5,6 +5,18 @@ def main(k,n):
         k: int
         n: int
     Returns:
+        list: return  answer   Repeat the number k n times and return to the list view.
+    Args:
+        k: int
+        n: int
+    Returns:
         list: return  answer
     """
-    return
+    for i in range(n):
+        print(k)
+(main(4,2))
+
+
+
+
+        
