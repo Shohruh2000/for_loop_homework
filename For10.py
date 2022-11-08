fbac0d906237cea7e840bbbfae2c533688c0e303
Shohruh2000 <shertoyloqovshohruh524@gmail.com>
2022-11-09 04:43:34 +0500

    For10.py

--- For10.py
+++ For10.py
@@ -1,9 +1,14 @@
 def main(list1):
     """
-    Returns a list where only the first letter of each name is capitalized.
-    Args:
-        list1: list
-    Returns:
-        list: return  answer
+Returns a list where only the first letter of each name is capitalized.
+Args:
+    list1: list
+Returns:
+    list: return  answer
     """
-    return
+    for i in ["SalohiDdIn","ManGuberdi","ShOhruh","UMId"]:
+        
+        print(i.title(),end=" ")
+    
+(main(["SalohiDdIn","ManGuberdi","ShOhruh","UMId"]))
+
